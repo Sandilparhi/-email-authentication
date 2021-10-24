c3f5adac84888762924c3dfb338eaa836d9833e0
sandil parhi <92135605+Sandilparhi@users.noreply.github.com>
2021-10-24 22:34:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');         // import express
+
+require('dotenv').config();               // import dotenv file 
+require('./config/database').connect();  //import database file 
+
+const RegisterRoute = require('./api/routes/register');  // import register route
+const LoginRoute = require('./api/routes/login');       // import login route
+const WelcomeRoute = require('./api/routes/welcome');       // import welcome route
+const ApproveRoute = require('./api/routes/approve');       // import approve route
+
+const app = express(); // configure exprss
+
+const { API_PORT } = process.env           // configure PORT
+const port = process.env.PORT || API_PORT // use port
+
+
+app.use(express.json()) // use express
+
+app.use('/register', RegisterRoute);     //use register route
+app.use('/login', LoginRoute);  //use login route
+app.use('/welcome', WelcomeRoute);  //use welcome route
+app.use('/approve', ApproveRoute);  //use approve route
+
+
+// run app using port
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
